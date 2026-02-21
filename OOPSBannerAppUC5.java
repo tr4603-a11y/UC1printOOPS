b9fc9846339a015689bd7c070d631ae138bc5895
tr4603-a11y <tr4603@srmist.edu.in>
2026-02-22 00:48:49 +0530

    Add files via upload

---
+++ OOPSBannerAppUC5.java
@@ -0,0 +1,21 @@
+public class OOPSBannerAppUC5 {
+
+    public static void main(String[] args) {
+
+        String[] banner = {
+
+            String.join(" ", "  *****  ", "  *****  ", " ******  ", "  ****** "),
+            String.join(" ", " *     * ", " *     * ", " *     * ", " *       "),
+            String.join(" ", " *     * ", " *     * ", " *     * ", " *       "),
+            String.join(" ", " *     * ", " *     * ", " ******  ", "  *****  "),
+            String.join(" ", " *     * ", " *     * ", " *       ", "       * "),
+            String.join(" ", " *     * ", " *     * ", " *       ", "       * "),
+            String.join(" ", "  *****  ", "  *****  ", " *       ", " ******  ")
+
+        };
+
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
