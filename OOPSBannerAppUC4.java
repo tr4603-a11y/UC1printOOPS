97e3ac1adb888d55c6eb4c58c5dd25086fda4fb5
tr4603-a11y <tr4603@srmist.edu.in>
2026-02-22 00:46:27 +0530

    Add files via upload

---
+++ OOPSBannerAppUC4.java
@@ -0,0 +1,45 @@
+public class OOPSBannerAppUC4 {
+
+    public static void main(String[] args) {
+
+        String O1 = "  *****  ";
+        String O2 = " *     * ";
+        String O3 = " *     * ";
+        String O4 = " *     * ";
+        String O5 = " *     * ";
+        String O6 = " *     * ";
+        String O7 = "  *****  ";
+
+        String P1 = " ******  ";
+        String P2 = " *     * ";
+        String P3 = " *     * ";
+        String P4 = " ******  ";
+        String P5 = " *       ";
+        String P6 = " *       ";
+        String P7 = " *       ";
+
+        String S1 = "  ****** ";
+        String S2 = " *       ";
+        String S3 = " *       ";
+        String S4 = "  *****  ";
+        String S5 = "       * ";
+        String S6 = "       * ";
+        String S7 = " ******  ";
+
+        // Create String array to store banner lines
+        String[] banner = {
+            String.join(" ", O1, O1, P1, S1),
+            String.join(" ", O2, O2, P2, S2),
+            String.join(" ", O3, O3, P3, S3),
+            String.join(" ", O4, O4, P4, S4),
+            String.join(" ", O5, O5, P5, S5),
+            String.join(" ", O6, O6, P6, S6),
+            String.join(" ", O7, O7, P7, S7)
+        };
+
+        // Loop to print banner
+        for (String line : banner) {
+            System.out.println(line);
+        }
+    }
+}
